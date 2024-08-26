bd631e03b667bf95492cb7e923228d2b986da14b
Mnighter <malik11112222333344445555@gmail.com>
2024-08-26 12:29:03 +0200

    Wat files toegevoegd
    ik he bgewoon de basis dingen snel bij te voegen

---
+++ head.php
@@ -0,0 +1,6 @@
+<head>
+	<title>Quiz-applicatie</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="fontawesome/css/all.min.css">
+</head>
